1871bd227d80d1df7ba9f67598455d8f2f0cfd2b
manoj9896 <manojkumar.nvsd@gmail.com>
2023-04-12 22:12:13 +0530

    initial setup

--- src/config.ts
+++ src/config.ts
@@ -5,11 +5,11 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.FireBase_API_KEY,
   authDomain: process.env.FireBase_AUTH_DOMAIN,
-  projectId: "ipl2023-league",
-  storageBucket: "ipl2023-league.appspot.com",
-  messagingSenderId: "857409743293",
-  appId: "1:857409743293:web:56903f4b5a230a4752cad4",
-  measurementId: "G-3SFBH4MQK2",
+  projectId: process.env.FireBase_Project_Id,
+  storageBucket: process.env.FireBase_Storage_Bucket,
+  messagingSenderId: process.env.FireBase_Messaging_Sender_Id,
+  appId: process.env.FireBase_App_Id,
+  measurementId: process.env.FireBase_Measurement_Id,
 };
 
 // Initialize Firebase
