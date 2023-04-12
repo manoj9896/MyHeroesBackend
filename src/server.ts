1ad5bca726116afed25edce1444e04b0de290e8b
manoj9896 <manojkumar.nvsd@gmail.com>
2023-04-12 23:56:09 +0530

    initial setup

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
-import baseRouter from "./routes/routes";
+import baseRouter from "./routes/routes"; 
 
 const app = express();
 const port = process.env.PORT;
