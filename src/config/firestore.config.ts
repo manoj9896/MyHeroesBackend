006952111aaad76077fe78d3d7398baf27c6cd4f
manoj9896 <manojkumar.nvsd@gmail.com>
2023-04-13 09:55:14 +0530

    inital setup

--- src/config/firestore.config.ts
+++ src/config/firestore.config.ts
@@ -1,18 +1,9 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import * as admin from "firebase-admin";
+const serviceAccount = require("../../serviceAccountKey.json");
 
-const firebaseConfig = {
-  apiKey: process.env.FireBase_API_KEY,
-  authDomain: process.env.FireBase_AUTH_DOMAIN,
-  projectId: process.env.FireBase_Project_Id,
-  storageBucket: process.env.FireBase_Storage_Bucket,
-  messagingSenderId: process.env.FireBase_Messaging_Sender_Id,
-  appId: process.env.FireBase_App_Id,
-  measurementId: process.env.FireBase_Measurement_Id,
-};
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-// Initialize Firebase
-export const firebase = initializeApp(firebaseConfig);
-export const auth = getAuth(firebase);
-export const db = getFirestore(firebase);
+export const db = admin.firestore();
+export const firebaseAuth = admin.auth();
